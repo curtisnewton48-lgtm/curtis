1e05d9068c170583b264fcafc520d4a0025d1769
curtisnewton48-lgtm <curtisnewton48@gmail.com>
2026-05-08 17:46:11 +0100

    Refactor indentation and formatting in auth.pyfix: auth.py indentation

--- career_agent/auth.py
+++ career_agent/auth.py
@@ -6,19 +6,19 @@ import os
 from google.oauth2 import service_account
 
 SCOPES = [
-      "https://www.googleapis.com/auth/spreadsheets",
-      "https://www.googleapis.com/auth/documents",
-      "https://www.googleapis.com/auth/drive.file",
+          "https://www.googleapis.com/auth/spreadsheets",
+          "https://www.googleapis.com/auth/documents",
+          "https://www.googleapis.com/auth/drive.file",
 ]
 
 
 def get_credentials():
-      raw_json = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
-      if raw_json:
-                info = json.loads(raw_json)
-                return service_account.Credentials.from_service_account_info(info, scopes=SCOPES)
+          raw_json = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
+          if raw_json:
+                        info = json.loads(raw_json)
+                        return service_account.Credentials.from_service_account_info(info, scopes=SCOPES)
 
-      path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-      if not path:
-                raise RuntimeError("Set GOOGLE_SERVICE_ACCOUNT_JSON or GOOGLE_APPLICATION_CREDENTIALS.")
-            return service_account.Credentials.from_service_account_file(path, scopes=SCOPES)
+          path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+          if not path:
+                        raise RuntimeError("Set GOOGLE_SERVICE_ACCOUNT_JSON or GOOGLE_APPLICATION_CREDENTIALS.")
+                    return service_account.Credentials.from_service_account_file(path, scopes=SCOPES)
